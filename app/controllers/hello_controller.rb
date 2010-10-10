e1472b6f79a6fd8b5a69828ccd37db0412a1134e
Grycek <adam.grycner@wp.pl>
2010-10-10 15:00:51 +0200

    Hello World w kontrolerze

--- app/controllers/hello_controller.rb
+++ app/controllers/hello_controller.rb
@@ -1,7 +1,5 @@
 class HelloController < ApplicationController
   def index
-  end
-  def world
-    @greeting = 'Hello world'
+    @greeting = 'Hello world!'
   end
 end
--- app/controllers/hello_world_controller.rb
+++
@@ -1,7 +0,0 @@
-class HelloWorldController < ApplicationController
-  def index
-  end
-  def world
-    @greeting = 'Hello world'
-  end
-end
